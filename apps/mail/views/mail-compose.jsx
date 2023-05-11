9afcad5f8a14d14c3164a3c115e5cb56a9fa57b5
Karin Zohar <karinzohar3@gmail.com>
2023-05-11 16:20:09 +0300

    built and designed compose cmp

--- apps/mail/views/mail-compose.jsx
+++ apps/mail/views/mail-compose.jsx
@@ -1,22 +1,27 @@
-
+const { useNavigate } = ReactRouterDOM
 export function MailCompose() {
+    const navigate = useNavigate()
+    
+    function closeCompose() {
+        navigate('/mail')
+    }
 
     return (
         <section className="mail-compose">
             <section className="compose-title">
                 <h2>New Message</h2>
                 <div>
-                    <button className="close-btn material-symbols-outlined"></button>
+                    <button className="close-btn material-symbols-outlined icon-bg" onClick={() => closeCompose()}></button>
                 </div>
             </section>
             <section>
                 <form >
-                    <article className="compose-to">
-                        <label htmlFor="to">To:</label>
+                    <article className="compose-to comp-field ">
+                        <label htmlFor="to">Recipients:</label>
                         <input type="text" name="to" id="to" />
                     </article>
 
-                    <article className="compose-subject">
+                    <article className="compose-subject comp-field ">
                         <label htmlFor="subject">Subject:</label>
                         <input type="text" name="subject" id="subject" />
                     </article>
@@ -24,10 +29,10 @@ export function MailCompose() {
                     <textarea className="mail-body" name="mail-body"></textarea>
 
                     <button className="send-btn">Send</button>
-                    <button className="delete-btn">Delete</button>
+                    <button className="delete-btn material-symbols-outlined icon-bg"></button>
 
-            </form>
-        </section>
+                </form>
+            </section>
         </section >
     )
 }
