2a7a86486999b0e101188e2e69f3f8ebf19653c5
Karin Zohar <karinzohar3@gmail.com>
2023-05-13 22:40:58 +0300

    improved home page

--- views/home.jsx
+++ views/home.jsx
@@ -1,13 +1,14 @@
+const { NavLink } = ReactRouterDOM
 
 export function Home() {
     return <section className="home">
-        <h1 className="logo">Welcome to Appsus.</h1>
+        <h1>Welcome to Appsus.</h1>
         <h3>The only organisation app you'll ever need</h3>
 
         <section className="home-btns">
-            <button>Mail</button>
-            <button>Keepster</button>
-            <button>Books</button>
+            <button><NavLink to="/mail">Mail</NavLink></button>
+            <button><NavLink to="/note">Keepster</NavLink></button>
+            <button><NavLink to="/book">Books</NavLink></button>
         </section>
     </section>
 }
