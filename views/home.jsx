df00d09222662da150102f7fc88db7620691bc29
Karin Zohar <karinzohar3@gmail.com>
2023-05-13 22:10:19 +0300

    Delivery final

--- views/home.jsx
+++ views/home.jsx
@@ -1,6 +1,13 @@
 
 export function Home() {
     return <section className="home">
-        <h1>Welcome to home page!</h1>
+        <h1 className="logo">Welcome to Appsus.</h1>
+        <h3>The only organisation app you'll ever need</h3>
+
+        <section className="home-btns">
+            <button>Mail</button>
+            <button>Keepster</button>
+            <button>Books</button>
+        </section>
     </section>
 }
