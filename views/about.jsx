dc5bcb71619e29f6b0654cc9ff9c4c312a746bad
Karin Zohar <karinzohar3@gmail.com>
2023-05-13 22:48:31 +0300

    added about page

--- views/about.jsx
+++ views/about.jsx
@@ -1,6 +1,13 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+        <h1>About</h1>
+        <article>
+            <p>Welcome to Appsus! Our goal is to help you become more efficient and effective in your daily life.</p>
+            <p>We understand that in today's fast-paced world, it can be challenging to balance work, family, and personal life. That's why we created this app to help you manage your time better and get things done.</p>
+            <p>Our app provides a variety of features to help you stay organized and focused. With Appsus, it's easier than ever to track your email, your notes and your reading lists all in one place. This way you can focus on the things that matter the most to you. </p>
+            <p>Whether you're a student, a professional, or a busy parent, Appsus can help you achieve your goals and make the most of your time.</p>
+            <p>We believe that productivity is the key to success, and our app is here to help you unlock your full potential. Try it out today and see how it can transform your life!</p>
+        </article>
     </section>
 }
